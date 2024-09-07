74af24a626ba6844ad8f197dff56e89f3a84b514
edmilton75 <edmilton7500@gmail.com>
2024-09-07 15:17:25 -0300

    exe 01

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -1,10 +1,22 @@
 //<reference types="Cypress" />
 
-describe('Central de Atendimento ao Cliente TAT', function() {
-    it('verifica o título da aplicação', function() {
-        cy.visit('./src/index.html')
+describe("Central de Atendimento ao Cliente TAT", function () {
+  beforeEach(function () {
+    cy.visit("./src/index.html");
+  });
 
-        cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
-    })
-  })
-   
+  it("verifica o título da aplicação", function () {
+    cy.title().should("be.equal", "Central de Atendimento ao Cliente TAT");
+  });
+
+  it.only("preenche os campos obrigatórios e envia o formulário", function () {
+    const longText = "teste testetestetestetestetestetestetestetestetesteteste";
+    cy.get("#firstName").type("Eddie");
+    cy.get("#lastName").type("Sousa");
+    cy.get("#email").type("edy@teste.com");
+    cy.get("#open-text-area").type(longText, { delay: 0 });
+    cy.get('button[type="submit"]').click();
+
+    cy.get(".success").should("be.visible");
+  });
+});
