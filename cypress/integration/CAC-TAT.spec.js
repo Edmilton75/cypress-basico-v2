7d29c5cda16f27c649798f3efec493a5f5fe87fe
edmilton75 <edmilton7500@gmail.com>
2024-09-21 22:42:29 -0300

    feat: teste aula 7 8 9

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -42,7 +42,7 @@ describe("Central de Atendimento ao Cliente TAT", function () {
     cy.get("#lastName").type("Sousa");
     cy.get("#phone");
     cy.get("#email").type("edteste@teste.com");
-    cy.get("#phone-checkbox").click();
+    cy.get("#phone-checkbox").check();
     cy.get("#open-text-area").type(longTexte, { delay: 0 });
     cy.contains("button", "Enviar").click();
     cy.get(".error").should("be.visible");
@@ -81,5 +81,69 @@ describe("Central de Atendimento ao Cliente TAT", function () {
     cy.filMandatoryFieldsAndSubmit();
 
     cy.get(".success").should("be.visible");
+  });
+
+  it("seleciona um produto (YouTube) por seu texto", function () {
+    cy.get("#product").select("YouTube").should("have.value", "youtube");
+  });
+
+  it("seleciona um produto (Metoria) por seu valor (value)", function () {
+    cy.get("#product").select("mentoria").should("have.value", "mentoria");
+  });
+
+  it("seleciona um produto (Blog) por seu índice", function () {
+    cy.get("#product").select(1).should("have.value", "blog");
+  });
+
+  it('marca o tipo de atendimento "Feedback"', function () {
+    cy.get("input[type=radio][value=feedback]")
+      .check()
+      .should("have.value", "feedback");
+  });
+
+  it("marca cada tipo de atendimento", function () {
+    cy.get("input[type=radio]")
+      .should("have.length", 3)
+      .each(($radio) => {
+        cy.wrap($radio).check();
+        cy.wrap($radio).should("be.checked");
+      });
+  });
+
+  it("seleciona um arquivo da pasta fixtures", function () {
+    cy.get("input[type='file']")
+      .should("not.have.value")
+      .selectFile("./cypress/fixtures/example.json")
+      .should(function ($input) {
+        expect($input[0].files[0].name).to.equal("example.json");
+      });
+  });
+
+  it("Selecione um arquivo simulando Drag-drop", function () {
+    cy.get("input[type=file]")
+      .should("not.have.value")
+      .selectFile("./cypress/fixtures/example.json", { action: "drag-drop" })
+      .should(function ($input) {
+        expect($input[0].files[0].name).to.equal("example.json");
+      });
+  });
+
+  it("seleciona um arquivo utilizando uma fixture para a qual foi dada um alias", function () {
+    cy.fixture("example.json").as("samplefile");
+    cy.get("input[type=file]")
+      .selectFile("@samplefile")
+      .should(function ($input) {
+        expect($input[0].files[0].name).to.equal("example.json");
+      });
+  });
+
+  it("verifica que a política de privacidade abre em outra aba sem a necessidade de um clique", function () {
+    cy.get("#privacy a").should("have.attr", "target", "_blank");
+  });
+
+  it("acessa a página da política de privacidade removendo o target e então clicando no link", function () {
+    cy.get("#privacy a").invoke("removeAttr", "target").click();
+
+    cy.contains("Talking About Testing").should("be.visible");
   });
 });
